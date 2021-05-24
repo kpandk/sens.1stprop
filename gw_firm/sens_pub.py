36aefa4d18f17f0929389e007b9936c1182dee7d
K Pan <a**********@gmail.com>
2021-05-24 05:46:02 +0100

    fixed argument pass

--- gw_firm/sens_pub.py
+++ gw_firm/sens_pub.py
@@ -13,13 +13,13 @@ class PUBLISH_CSV:
         with open("/home/dpan/.sens_data/realtime_sens.csv", "r") as sens:
             sens_data = sens.readlines()[-1]
         return sens_data
-    def publish_to_client(self, data_str):
+    def publish_to_client(self):
         client = mqtt_client.Client()
         #client.is_callback = self.is_callback(client, userdata, mid)
         client.connect(self.server, self.port, keepalive=60, bind_address="")
         client.loop_start()
         while True:
-            (rc, mid) = client.publish(self.topic, str(data_str), self.qos)
+            (rc, mid) = client.publish(self.topic, str(self.handle_csv_file()), self.qos)
             if rc == 0:
                 logging.info('published OK')
             else:
@@ -30,7 +30,7 @@ class PUBLISH_CSV:
 
 def main():
     pub_data = PUBLISH_CSV("poc/sens", 0, "localhost", 1883)
-    data_str = pub_data.publish_to_client(pub_data.handle_csv_file())
+    data_str = pub_data.publish_to_client()
 
 if __name__ == '__main__':
     import logging
